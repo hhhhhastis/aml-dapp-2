878029381aaddb57d57cc7df534932bf16e2202f
hhhhhasti <mlbbeeca111@gmail.com>
2026-03-22 08:22:21 +0300

    feat: approve+pay flow with AMLPayment contract (Nile testnet)

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import '@/styles/globals.css';
 import { Toaster } from 'react-hot-toast';
 
+useEffect(() => {
+  if (typeof window !== 'undefined') {
+    import('eruda').then(e => e.default.init());
+  }
+}, []);
+
 export default function App({ Component, pageProps }) {
   return (
     <>
